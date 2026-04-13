60101bfcae94f807cc85151532a86659e1564e2d
titoValen <valentin.tito@davinci.edu.ar>
2026-04-13 15:25:36 -0300

    Add head component with meta tags and stylesheets

---
+++ components/head.php
@@ -0,0 +1,9 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+  <title>Parcial Programación II nº1</title>
+
+  <link rel="stylesheet" href="style/gobal.css">
+  <link rel="stylesheet" href="style/index.css">
+</head>
