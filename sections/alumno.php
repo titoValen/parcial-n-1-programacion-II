b14a3416f9f9d1b15f7e3db416a5799091d8aaf9
titoValen <valentin.tito@davinci.edu.ar>
2026-04-14 15:42:30 -0300

    Agregar sección de datos del alumno en alumno.php

---
+++ sections/alumno.php
@@ -0,0 +1,29 @@
+<main>
+  <h1>Datos del Alumno</h1>
+
+  <div>
+    <h2>Nombre: Tito Valentín</h2>
+    <h2>Edad: 20 años</h2>
+    <h2>Correo: valentin.tito@davinci.edu.ar</h2>
+    <ul>
+      <li>
+        <a target="_blank" href="https://github.com/DonValen910">
+          <figure><img src="./img/icon/GitHub_light.svg" alt="GitHub"></figure>
+        </a>
+      </li>
+      <li>
+        <a target="_blank" href="mailto:valentin.tito@davinci.edu.ar">
+          <figure><img src="./img/icon/gmail.svg" alt="Email"></figure>
+        </a>
+      </li>
+      <li>
+        <a target="_blank" href="https://www.linkedin.com/in/valentin-tito">
+          <figure><img src="./img/icon/linkedin.svg" alt="LinkedIn"></figure>
+        </a>
+      </li>
+    </ul>
+    <figure>
+      <img src="./img/photo/alumno.jpeg" alt="Foto del Alumno">
+    </figure>
+  </div>
+</main>
