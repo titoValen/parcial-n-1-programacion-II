163d8c61550095a268e8f75ed11a88e17840ba9d
titoValen <valentin.tito@davinci.edu.ar>
2026-04-14 15:37:29 -0300

    Agregar lógica para cargar vistas dinámicamente en index.php

--- index.php
+++ index.php
@@ -2,7 +2,21 @@
 
 <body>
   <?php require_once 'components/header.php'; ?>
-  <main></main>
+  <?php
+  $vistas_creadas = ['home', 'producto', 'contacto', 'alumno'];
+  if (
+    isset($_GET['vista']) &&
+    !empty($_GET['vista']) &&
+    in_array($_GET['vista'], $vistas_creadas)
+  ) {
+    $vista = $_GET['vista'];
+  } else {
+    $vista = 'home';
+  }
+
+  require_once "sections/$vista.php";
+
+  ?>
   <?php require_once 'components/footer.php'; ?>
 </body>
 
