c3ce6034ed3a0b5f5eb0dd890faccac68f7002e4
titoValen <valentin.tito@davinci.edu.ar>
2026-04-20 16:02:06 -0300

    Mejorar el diseño de las tarjetas de productos con nuevos estilos y estructura en card.php y index.css

--- components/card.php
+++ components/card.php
@@ -4,8 +4,15 @@ foreach ($datos as $producto => $items) {
     $price = number_format($item["price"], 0, ",", ".");
 
     echo "<div class='card' id='$item[id]'>";
-    echo "<h2>$item[name]</h2>";
-    echo "<p>Precio: $price</p>";
+    echo "<div class='card-header'>";
+    echo "<span class='card-price'>$$price</span>";
+    echo "</div>";
+    echo "<div class='card-body'>";
+    echo "<figure>";
+    echo "<img src='$item[image]' alt='$item[alt]'>";
+    echo "</figure>";
+    echo "<h2 class='card-title'>$item[name]</h2>";
+    echo "</div>";
     echo "</div>";
   }
 }
