d76fe4b10313ffdddfe934e6c331d91c12b250b5
titoValen <valentin.tito@davinci.edu.ar>
2026-04-20 16:20:12 -0300

    Eliminar encabezado y agregar estilos CSS para mejorar la presentación en home.php

--- sections/home.php
+++ sections/home.php
@@ -1,7 +1,15 @@
 <main>
-  <h1>Home</h1>
-
   <?php
   require_once "components/card.php";
   ?>
 </main>
+
+<style>
+  main {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 1.5rem;
+    margin: 1.5rem;
+  }
+</style>
