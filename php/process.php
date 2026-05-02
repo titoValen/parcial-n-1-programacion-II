87dce337e2f1ef01389b85cfd261c1ce278509cc
titoValen <valentin.tito@davinci.edu.ar>
2026-05-02 00:28:36 -0300

    Eliminar el archivo process.php y su lógica de carga de productos

--- php/process.php
+++
@@ -1,9 +0,0 @@
-<?php
-$archivo = "data/products.json";
-
-if (file_exists($archivo)) {
-  $json = file_get_contents($archivo);
-  $datos = json_decode($json, true);
-} else {
-  $datos = "El archivo no existe";
-}
